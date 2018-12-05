d25dbfe84a691916647df23b825b00c6162c620b
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-05 16:05:58 +0100

    working setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         "Framework :: Pytest",
-    ]
+    ],
 )
