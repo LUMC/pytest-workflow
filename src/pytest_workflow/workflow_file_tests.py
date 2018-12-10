38088c52efc86bebc6f3f3379f96abbc34d58da1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-10 16:35:11 +0100

    build a files exist collector

--- src/pytest_workflow/workflow_file_tests.py
+++ src/pytest_workflow/workflow_file_tests.py
@@ -8,12 +8,20 @@ class WorkflowFilesTestCollector(pytest.Collector):
         self.files = files
 
 class FilesExistCollector(pytest.Collector):
-    def __init__(self, files: List[Path]):
+    def __init__(self, name, files: List[Path]):
         self.files = files
+        self.name = name
 
     def collect(self):
         for file in self.files:
-            yield FileExists(file)
+            name = "{0}. File exists: {1}".format(self.name, file)
+            yield FileExists(name, self, file)
 
 class FileExists(pytest.Item):
-    def __init__(self, name, parent, files: List[dict]):
+    def __init__(self, name, parent, file: Path):
+        super(FileExists, self).__init__(name, parent)
+        self.file = file
+
+    def runtest(self):
+        assert self.file.exists()
+
