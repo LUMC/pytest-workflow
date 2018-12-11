fbe7a27715d7638608e73dc5920faac04bfda2c8
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-11 17:32:47 +0100

    formatting

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,21 +1,25 @@
+from pathlib import Path
+
 from pytest_workflow.workflow import Workflow
 
+
 def test_stdout():
-    workflow = Workflow("echo",["moo"])
+    workflow = Workflow(Path("echo"), ["moo"])
     workflow.run()
     assert workflow.stdout == b"moo\n"
 
+
 def test_exit_code():
-    workflow = Workflow("echo",["moo"])
+    workflow = Workflow(Path("echo"), ["moo"])
     workflow.run()
     assert workflow.exit_code == 0
-    workflow2 = Workflow("grep", [])
+    workflow2 = Workflow(Path("grep"), [])
     workflow2.run()
     assert workflow2.exit_code == 2
 
+
 def test_stderr():
     # This will fail and print a shortened grep usage.
-    workflow = Workflow("grep", [])
+    workflow = Workflow(Path("grep"), [])
     workflow.run()
     assert "grep" in str(workflow.stderr)
-
