ef568eae72968c45c29a930d68118c2d757e0f80
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-12 14:38:28 +0100

    commented the file tests file

--- src/pytest_workflow/workflow_file_tests.py
+++ src/pytest_workflow/workflow_file_tests.py
@@ -7,35 +7,60 @@ import pytest
 
 
 class WorkflowFilesTestCollector(pytest.Collector):
+    """Collects all the files related tests"""
     def __init__(self, name, parent, files: List[dict], cwd):
+        """
+        A WorkflowFilesTestCollector starts all the files-related tests
+        :param name: The name of the tests
+        :param parent: The collector that started this collector
+        :param files: A list of `files` which are dictionaries in the schema
+        the dictionaries define the tests.
+        :param cwd: The directory relative to which relative file paths will
+        be tested.
+        """
         self.files = files
         self.name = name
         self.cwd = cwd
         super(WorkflowFilesTestCollector, self).__init__(name, parent=parent)
 
     def collect(self):
+        """Starts all file related tests"""
         filepaths = [Path(x.get("path")) for x in self.files]
         # Structure why not the file exists directly?
         # Because also some other operations on files will be added to
-        # this list.
+        # this list. Like contains, md5sum etc.
         return [FilesExistCollector(self.name, self, filepaths, self.cwd)]
 
 
 class FilesExistCollector(pytest.Collector):
+    """Spawns tests to check for files existence"""
     def __init__(self, name, parent, files: List[Path], cwd):
+        """
+        :param name: Name of the test.
+        :param parent: Collector that started this test.
+        :param files: A list of paths to be tested.
+        :param cwd: The directory relative to which relative paths are tested.
+        """
         self.files = files
         self.name = name
         self.cwd = cwd
         super(FilesExistCollector, self).__init__(name, parent=parent)
 
     def collect(self):
+        """Starts all the file existence tests."""
         for test_file in self.files:
             name = "{0}. File exists: {1}".format(self.name, test_file)
             yield FileExists(name, self, Path(self.cwd / test_file))
 
 
 class FileExists(pytest.Item):
+    """A pytest file exists test."""
     def __init__(self, name, parent, file: Path):
+        """
+        :param name: Test name
+        :param parent: Collector that started this test
+        :param file: A path to the file
+        """
         super(FileExists, self).__init__(name, parent)
         self.file = file
 
