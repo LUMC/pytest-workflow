bbb39259e6296b36e5161fb9833f4f04db07f18d
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-17 13:53:01 +0100

    have all types  correct

--- src/pytest_workflow/schema.py
+++ src/pytest_workflow/schema.py
@@ -18,7 +18,7 @@
 
 import json
 from pathlib import Path
-from typing import NamedTuple, NewType, List, Type
+from typing import NamedTuple, NewType, List, Type, Optional
 
 import jsonschema
 
@@ -41,20 +41,22 @@ def validate_schema(instance):
 
 ####### Schema classes below
 ####### These should be dataclasses. But that's not supported in python<3.7
-class StdOutErr(NamedTuple):
+class ContentCheck(NamedTuple):
     def __init__(self, contains: List[str] = [],
                  must_not_contain: List[str] = []):
         pass
 
 
-class FileTest(NamedTuple):
-    def __init__(self):
+class FileTest(ContentCheck):
+    def __init__(self, path: str, md5sum: Optional[str] = None,
+                 contains: List[str] = [], must_not_contain: List[str] = []):
+        super.__init__(contains=contains, must_not_contain=must_not_contain)
         pass
 
 
 class WorkflowTest(NamedTuple):
     def __init__(self, name: str, command: str, exit_code: int = 0,
-                 stdout: Type[StdOutErr] = StdOutErr(),
-                 stderr: Type[StdOutErr] = StdOutErr(),
+                 stdout: Type[ContentCheck] = ContentCheck(),
+                 stderr: Type[ContentCheck] = ContentCheck(),
                  files: List[Type[FileTest]] = []):
         pass
