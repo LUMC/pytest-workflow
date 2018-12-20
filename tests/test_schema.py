66f55544e7e89205a9e62243954e53cf381f4c76
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-20 13:35:31 +0100

    fix yaml safe load

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -95,7 +95,7 @@ contains_list = [
 
 
 @pytest.mark.parametrize("instance",
-                         [yaml.load(workflow) for workflow in contains_list])
+                         [yaml.safe_load(workflow) for workflow in contains_list])
 def test_validate_schema_contains_conflict(instance):
     with pytest.raises(jsonschema.ValidationError) as e:
         validate_schema(instance)
