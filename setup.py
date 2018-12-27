371131f588ac7247cdbce8052f4b7ec8d030e4c1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2018-12-27 17:09:34 +0100

    change version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="pytest-workflow",
-    version="0.1.0",
+    version="0.2.0-dev",
     description="A pytest plugin for configuring workflow/pipeline tests "
                 "using YAML files",
     author="Leiden University Medical Center, various departments",
