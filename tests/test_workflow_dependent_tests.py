742ba3a1831ed0d69b5c9f8200bd1b997ea866e4
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-02-07 15:52:47 +0100

    add a few sanity tests

--- tests/test_workflow_dependent_tests.py
+++ tests/test_workflow_dependent_tests.py
@@ -28,14 +28,6 @@ def test_hook_impl():
     assert True
 """)
 
-TEST_FIXTURE = textwrap.dedent("""\
-import pytest
-
-@pytest.mark.workflow(name="simple echo")
-def test_fixture_impl(workflow_dir):
-    assert workflow_dir.name == "simple_echo"
-""")
-
 
 def test_not_skipped(testdir):
     testdir.makefile(".yml", test_simple=SIMPLE_ECHO)
@@ -54,8 +46,64 @@ def test_skipped(testdir):
     assert "'simple echo' has not run." in result.stdout.str()
 
 
+TEST_FIXTURE = textwrap.dedent("""\
+import pytest
+
+@pytest.mark.workflow(name="simple echo")
+def test_fixture_impl(workflow_dir):
+    assert workflow_dir.name == "simple_echo"
+""")
+
+
 def test_workflow_dir_arg(testdir):
     testdir.makefile(".yml", test_simple=SIMPLE_ECHO)
     testdir.makefile(".py", test_fixture=TEST_FIXTURE)
     result = testdir.runpytest()
     result.assert_outcomes(passed=5, failed=0, error=0, skipped=0)
+
+
+def test_worfklow_dir_arg_skipped(testdir):
+    """Run this test to check if this does not run into fixture request
+    errors"""
+    testdir.makefile(".yml", test_simple=SIMPLE_ECHO)
+    testdir.makefile(".py", test_fixture=TEST_FIXTURE)
+    result = testdir.runpytest("-v", "-r", "s", "--tag", "flaksdlkad")
+    result.assert_outcomes(skipped=1)
+
+
+TEST_FIXTURE_WORKFLOW_NOT_EXIST = textwrap.dedent("""\
+import pytest
+
+@pytest.mark.workflow(name="shoobiedoewap")
+def test_fixture_impl(workflow_dir):
+    assert workflow_dir.name == "simple_echo"
+""")
+
+
+def test_worfklow_not_exist_dir_arg(testdir):
+    """Run this test to check if this does not run into fixture request
+    errors"""
+    testdir.makefile(".yml", test_simple=SIMPLE_ECHO)
+    testdir.makefile(".py", test_fixture=TEST_FIXTURE_WORKFLOW_NOT_EXIST)
+    result = testdir.runpytest("-v", "-r", "s")
+    result.assert_outcomes(skipped=1, passed=4)
+    assert "'shoobiedoewap' has not run." in result.stdout.str()
+
+
+TEST_FIXTURE_UNMARKED_TEST = textwrap.dedent("""\
+import pytest
+
+def test_fixture_impl(workflow_dir):
+    assert workflow_dir.name == "simple_echo"
+""")
+
+
+def test_fixture_unmarked_test(testdir):
+    """Run this test to check if this does not run into fixture request
+    errors"""
+    testdir.makefile(".yml", test_simple=SIMPLE_ECHO)
+    testdir.makefile(".py", test_fixture=TEST_FIXTURE_UNMARKED_TEST)
+    result = testdir.runpytest("-v", "-r", "s")
+    result.assert_outcomes(error=1, passed=4)
+    assert ("workflow_dir can only be requested in tests marked with "
+            "the workflow mark.") in result.stdout.str()
