fb39629cdb393da08d049e68c06adb063a0f1e98
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2019-03-05 08:41:11 +0100

    add production/stable qualifier

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: "
         "GNU Affero General Public License v3 or later (AGPLv3+)",
         "Framework :: Pytest",
